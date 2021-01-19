ecf846d4530564d557961d6fee11386de7e290ef
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2021-01-19 14:31:10 -0300

    finalizando javaScript

--- js/index.js
+++ js/index.js
@@ -1,15 +1,35 @@
 function resposta() {
-  let pais = window.document.getElementsByName("pais");
-  validarResposta(pais);
+  let pais = document.getElementsByName("pais");
+  let resposta = document.getElementById("resposta");
+  let resposta1 = document.getElementById("resposta1");
+
+  validarResposta(pais, resposta, resposta1);
 }
 
-function validarResposta(pais) {
+let contadorAcertos = 0;
+let contadorErros = 0;
+
+function validarResposta(pais, resposta, resposta1) {
   if (pais[0].checked) {
-    alert("errou");
+    contadorErros += 1;
+    resposta.innerHTML = "ERROU!";
+    resposta.style.background = "red";
   } else if (pais[1].checked) {
-    alert("errou");
+    contadorErros += 1;
+    resposta.innerHTML = "ERROU!";
+    resposta.style.background = "red";
   } else if (pais[2].checked) {
-    alert("Acertou");
+    contadorAcertos += 1;
+    resposta.innerHTML = "ACERTOU";
   } else if (pais[3].checked) {
+    contadorAcertos += 1;
+    resposta1.innerHTML = "ACERTOU";
   }
+
+  return pais, resposta;
+}
+
+function finalizar(acerto, erro) {
+  let resultado = document.getElementById("finalizar");
+  resultado.innerHTML = `Acertos: ${acerto} e Erros: ${erro}`;
 }
